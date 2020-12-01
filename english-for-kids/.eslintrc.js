0432a0f9eb809b995bc5cb4f14850dd677ec1912
vmg999 <vmg999@gmail.com>
2020-12-01 20:15:39 +0200

    feat: implement project structure

---
+++ english-for-kids/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    // "extends": "eslint:recommended",
+    "extends": "airbnb",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
---
+++ english-for-kids/webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+const CopyWebpackPlugin= require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+      new CopyWebpackPlugin({
+          patterns: [
+            {
+                from: './src/audio',
+                to: './audio'
+            },
+            {
+            from: './src/img',
+            to: './img'
+            },
+            {
+            from: './src/index.html',
+            to: './index.html'
+            }
+      ]}),
+    ]
+};
