9782aacbd3dc6998e237469f42b6f643a5327f5f
vmg999 <vmg999@gmail.com>
2020-12-11 17:02:44 +0200

    feat: add card roatate

--- english-for-kids/src/js/newcard.js
+++ english-for-kids/src/js/newcard.js
@@ -1,39 +1,71 @@
 import { closeburger } from "./burger";
 import cards from './cards';
 
+let isRotate = false;
+
 export function createCard(obj) {
+
     let card = document.createElement('div');
-    card.classList.add('setcard');
+    card.classList.add('card');
     card.setAttribute('id', `card_${obj.word}`);
 
+    let front = document.createElement('div');
+    front.classList.add('front');
+
     const divimg = document.createElement('div');
-    divimg.classList.add('cardimg')
-    const img = document.createElement('img');
-    img.setAttribute('src', obj.image);
-    img.classList.add('setimg');
+    divimg.classList.add('cardimage');
+    divimg.style.backgroundImage = `url('${obj.image}')`;
 
     const word = document.createElement('p');
     word.textContent = obj.word;
 
+    const rotate = document.createElement('div');
+    rotate.classList.add('rotate-btn');
+    rotate.style.backgroundImage = "url('../img/rotate.png')";
+    rotate.addEventListener('click', (e)=>{
+        rotate.classList.toggle('hide');
+        e.stopPropagation();
+        card.classList.toggle('rotate-card');
+        isRotate = true;
+    })
+
+    front.append(divimg);
+    front.append(word);
+    front.append(rotate);
+
     const audio = document.createElement('audio');
     audio.setAttribute('src', obj.audioSrc);
 
-    const rotate = document.createElement('div');
-    rotate.classList.add('rotate');
-    rotate.addEventListener('click', ()=>{
-        card.classList.toggle('rotate-card');
-    })
+    let back = document.createElement('div');
+    back.classList.add('back');
     
-    divimg.append(img);
-    card.append(divimg);
-    card.append(word);
+    const backdivimg = document.createElement('div');
+    backdivimg.classList.add('cardimage');
+    backdivimg.style.backgroundImage = `url('${obj.image}')`;
+
+    const backword = document.createElement('p');
+    backword.textContent = obj.translation;
+
+    back.append(backdivimg);
+    back.append(backword);
+    
+    card.append(front);
     card.append(audio);
-    card.append(rotate);
+    card.append(back);
+    
 
     card.addEventListener('click', ()=>{
       audio.currentTime = 0;
       audio.play();
       closeburger();
+    })
+
+    card.addEventListener('mouseleave', ()=>{
+        if(isRotate){
+            isRotate = false;
+            card.classList.toggle('rotate-card');
+            rotate.classList.toggle('hide');
+        }
     })
 
     return card;
